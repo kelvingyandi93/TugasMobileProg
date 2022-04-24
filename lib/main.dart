daece22225d88f541773a90d518e7fda92ccbc7f
kelvin gyandi <kelvingyandi93@gmail.com>
2022-04-24 14:02:48 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: Column(
             children: [
+              Image(image: AssetImage('Assets/2.png'),height: 300),
               Image(image: NetworkImage('https://img.i-scmp.com/cdn-cgi/image/fit=contain,width=1098,format=auto/sites/default/files/styles/1200x800/public/d8/images/methode/2020/06/08/4b0bdfc6-a639-11ea-8ea0-d7434be00753_image_hires_121748.jpg?itok=44SyZaXz&v=1591589877')),
               Text('Jisoo', style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),),
-              Image(image: AssetImage('Assets/1.png'), width: 150, height:  350,),  
             ],
           ),
           ),
